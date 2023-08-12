1fcdb278ac1be575e32e6bfcf1f54b51b0731739
nurcanNT <nrcntpkra@gmail.com>
2023-08-12 16:15:46 +0300

    select example update

--- selectWithNt/script.js
+++ selectWithNt/script.js
@@ -1,20 +1,6 @@
-const app = Vue.createApp({
-    components: {
-      NtSelect: window["nt-select"]
-    },
-    data() {
-      return {
-        books: [
-          { title: "HTML5 Books" },
-          { title: "The Lock Artist" },
-          { title: "The Man's War", icon: "name" },
-          { title: "Thank You Jeeves" }
-        ]
-      };
-    }
-  });
-  
-  app.mount("#app");
-  
+const bookSelect = document.getElementById("bookSelect");
+const selectedBook = document.getElementById("selectedBook");
 
-console.log(app);
+bookSelect.addEventListener("change", function() {
+    selectedBook.textContent = bookSelect.value;
+});
