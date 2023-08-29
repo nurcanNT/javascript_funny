dfc8d166acc3c0723084c67f32c266d6fa8fe680
nurcanNT <nrcntpkra@gmail.com>
2023-08-29 23:39:39 +0300

    timeout update

--- timeSensitiveBackground/script.js
+++ timeSensitiveBackground/script.js
@@ -1,68 +1,72 @@
-import * as SunCalc from  "https://cdn.skypack.dev/suncalc@1.9.0";
-
 const r = document.querySelector(':root');
 const clockElement = document.getElementById('Clock');
 const nightToggleElement = document.getElementById('NightToggle');
 
-/*when checkbow is clicked*/
-nightToggleElement.addEventListener('change', ()=>{
-    const isCheched = nightToggleElement.checked;
-    const colorValue = isCheched ? "#eee" : "#111";
-    const brightnessValue = isCheched ? "0.5" : "2.4";
-    const hueRotateValue = isCheched ? "179deg" : "168deg";
+/* Saat güncelleme mekanizması */
+function clock() {
+    const now = new Date();
 
-    /*update CSS variables (immediately and after 2.5s)*/
+    const formatDateOptions = {
+        hour12: true,
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+    };
+
+    clockElement.innerHTML = now.toLocaleTimeString('en-GB', formatDateOptions);
+}
+
+clock(); // Saati başlatın
+setInterval(clock, 1000); // Saati her saniye güncelleyin
+
+/* Night Toggle */
+nightToggleElement.addEventListener('change', () => {
+    const isChecked = nightToggleElement.checked;
+    const colorValue = isChecked ? "#eee" : "#111";
+    const brightnessValue = isChecked ? "0.5" : "2.4";
+    const hueRotateValue = isChecked ? "179deg" : "168deg";
+
     r.style.setProperty('filter-opacity', "0");
     setTimeout(() => {
         r.style.setProperty('--color', colorValue);
         r.style.setProperty('--filter-brightness', brightnessValue);
         r.style.setProperty('--filter-hue-rotate', hueRotateValue);
         r.style.setProperty('--filter-opacity', "1");
-    }, 2500);
-   
+    }, 3500);
 });
-//time sensitive
 
-function getPrecision(value, min, max) {
-    const interval = max - min;
-    const distanceValue = value - min;
-    return Math.abs(Math.round(100 - distanceValue / (interval /2) * 100));
-}
-
-//fetch the geographical coordinates of the user
+/* Geolocation ve Saat Güncelleme */
 navigator.geolocation.getCurrentPosition((position) => {
-    const {latitude, longitude}= position.coords;
-
-    //use UseCalc to Calculate Sunrise Time
+    const { latitude, longitude } = position.coords;
 
     const now = new Date();
-    const times = SunCalc.getTimes(new Date(), latitude, longitude);
+    const times = SunCalc.getTimes(now, latitude, longitude);
     const sunrise = new Date(times.sunrise);
     const sunset = new Date(times.sunset);
-    const delayTime = 30 * 60 * 1000; 
+    const delayTime = 30 * 60 * 1000;
     const sunriseStart = new Date(sunrise.getTime() - delayTime);
-    const sunriseEnd = new Date(sunrise.getTime()+delayTime);
+    const sunriseEnd = new Date(sunrise.getTime() + delayTime);
     const sunsetStart = new Date(sunset.getTime() - delayTime);
-    const sunsetEnd = new Date(sunset.getTime()+ delayTime);
-    const interval = delayTime * 2;
-    const sunriseDistance = now - sunriseStart;
-    const sunsetDistance = now - sunsetStart;
-    const isNight = now < sunrise || now > sunset;
+    const sunsetEnd = new Date(sunset.getTime() + delayTime);
 
+    const isNight = now < sunrise || now > sunset;
     const colorValue = isNight ? '#eee' : '#111';
     const brightnessValue = isNight ? "0.5" : "2.4";
     const hueRotateValue = isNight ? "179deg" : "168deg";
-    const opacityValue = now > sunriseStart && now < sunriseEnd 
-    ? getPrecision(now, sunriseStart, sunriseEnd)
-    : now > sunsetStart && now < sunsetEnd
-    ? getPrecision(now, sunsetStart, sunsetEnd): 100;
+
+    let opacityValue = 100;
+    if (now > sunriseStart && now < sunriseEnd) {
+        opacityValue = getPrecision(now, sunriseStart, sunriseEnd);
+    } else if (now > sunsetStart && now < sunsetEnd) {
+        opacityValue = getPrecision(now, sunsetStart, sunsetEnd);
+    }
 
     console.log({
-        isNight: now < sunrise || now > sunset,
-        sunriseStart,
-        sunriseEnd,
-        now,
-        opacityValue
+        isNight: isNight,
+        sunriseStart: sunriseStart,
+        sunriseEnd: sunriseEnd,
+        now: now,
+        opacityValue: opacityValue
     });
 
     r.style.setProperty('--color', colorValue);
@@ -70,19 +74,3 @@ navigator.geolocation.getCurrentPosition((position) => {
     r.style.setProperty('--filter-hue-rotate', hueRotateValue);
     r.style.setProperty('--filter-opacity', opacityValue + "%");
 });
-
-function clock() {
-    const now = new Date();
-
-    const formatDateOptios = {
-        hour12: true,
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-    };
-
-    clockElement.innerHTML = now.toLocaleTimeString('en-GB', formatDateOptios);
-
-    clock();
-    setInterval(clock,1000);
-}
